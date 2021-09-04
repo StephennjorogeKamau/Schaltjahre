3f890116aadb9b60d40474d77de7bc6be272948b
StephennjorogeKamau <stevenjosh19@yahoo.com>
2021-09-04 18:37:12 +0200

    Create MainWindow.xaml.cs

---
+++ ADO-NET-Eibführung/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace ADO_NET_Eibführung
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //
+        string conStr = "Data Source=((localdb)\\MSSQLLocalDB);DataBase = Mitarbeiterprojekt.mdf; Connect Timeout=30;Integrated Security=true";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //DATABASE anfragen
+        private void btAbfrage_Click(object sender, RoutedEventArgs e)
+        {
+
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = conStr;
+            
+
+            try
+            {
+                con.Open();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+                return;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+       
+    }
+}
