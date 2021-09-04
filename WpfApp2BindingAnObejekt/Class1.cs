7d04313e99049221ac94744dee81e43772862b3a
StephennjorogeKamau <stevenjosh19@yahoo.com>
2021-09-04 18:41:32 +0200

    Create Class1.cs

---
+++ WpfApp2BindingAnObejekt/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp2BindingAnObejekt
+{
+    class Class1 : MainWindow
+    {
+        public int MyProgress { get; set; }
+        public Class1()
+        {
+            InitializeComponent();
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (slider1.Value > 50) txb2.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+        }
+    }
+}
