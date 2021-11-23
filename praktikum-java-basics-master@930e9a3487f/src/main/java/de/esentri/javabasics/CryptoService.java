1fc3ae51e14532360c06d24a46750ea92b24d632
sStephen Kamau <stephenkamau@esentri.com>
2021-11-23 05:51:07 +0100

    Update CryptoService.java

--- praktikum-java-basics-master@930e9a3487f/src/main/java/de/esentri/javabasics/CryptoService.java
+++ praktikum-java-basics-master@930e9a3487f/src/main/java/de/esentri/javabasics/CryptoService.java
@@ -4,6 +4,18 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CryptoService {
+  Scanner in = new Scanner (System.in);
+   System.out.print("Praktikum");
+
+  String userMessage = in.nextLine();
+  System.out.print("Ganzzahliger Wert zwischen 1 und 6 für den Verschlüsselungscode: ");
+  int userKey = in.nextInt();
+
+  while (userKey > 6) {
+    System.out.print("ERROR! Bitte nur 1 - 6: ");
+    userKey = in.nextInt();
+
+}
 
   /*
    * Diese Methode soll den Input-Parameter encrypten, also verschlüsseln.
@@ -11,6 +23,12 @@ public class CryptoService {
    * Beispiel: Für den Input "Praktikum" soll der Wert "Uwfpynpzr" zurückgegeben werden.
    */
   public String encrypt(String input) {
+    String input = userMessage;
+    int key = userKey;
+    char [] chars = input.toCharArray();
+    for(char i : chars) {
+      i += key;
+      System.out.print(i);
     return input;
   }
 
@@ -20,6 +38,12 @@ public class CryptoService {
    * * Beispiel: Für den Input "Uwfpynpzr" soll der Wert "Praktikum" zurückgegeben werden.
    */
   public String decrypt(String input) {
+      String input = userMessage;
+      int key = userKey;
+      char [] chars = input.toCharArray();
+      for(char i : chars) {
+        i -= key;
+        System.out.print(i);
     return input;
   }
 }
