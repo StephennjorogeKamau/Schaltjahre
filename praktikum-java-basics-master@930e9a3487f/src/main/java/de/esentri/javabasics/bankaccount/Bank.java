35375f4367454fcd1c3de358888701ddafea3cad
sStephen Kamau <stephenkamau@esentri.com>
2021-11-23 15:15:27 +0100

    Update Bank.java

--- praktikum-java-basics-master@930e9a3487f/src/main/java/de/esentri/javabasics/bankaccount/Bank.java
+++ praktikum-java-basics-master@930e9a3487f/src/main/java/de/esentri/javabasics/bankaccount/Bank.java
@@ -7,4 +7,34 @@ public class Bank {
     private String bic;
     private String name;
     private List<BankAccount> bankAccounts;
+
+    public Bank(String bic, String name, List<BankAccount> bankAccounts) {
+        this.bic = bic;
+        this.name = name;
+        this.bankAccounts = bankAccounts;
+    }
+
+    public String getBic() {
+        return bic;
+    }
+
+    public void setBic(String bic) {
+        this.bic = bic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<BankAccount> getBankAccounts() {
+        return bankAccounts;
+    }
+
+    public void setBankAccounts(List<BankAccount> bankAccounts) {
+        this.bankAccounts = bankAccounts;
+    }
 }
