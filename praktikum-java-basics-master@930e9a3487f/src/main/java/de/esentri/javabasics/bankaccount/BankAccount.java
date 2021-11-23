7de7090056b9be1f944b2e0b19f863db368a2106
sStephen Kamau <stephenkamau@esentri.com>
2021-11-23 15:15:32 +0100

    Update BankAccount.java

--- praktikum-java-basics-master@930e9a3487f/src/main/java/de/esentri/javabasics/bankaccount/BankAccount.java
+++ praktikum-java-basics-master@930e9a3487f/src/main/java/de/esentri/javabasics/bankaccount/BankAccount.java
@@ -13,4 +13,52 @@ public class BankAccount {
     private double balance;
     private Bank bank;
     private Customer customer;
+
+    public BankAccount(String iban, int pin, double balance, Bank bank, Customer customer) {
+        this.iban = iban;
+        this.pin = pin;
+        this.balance = balance;
+        this.bank = bank;
+        this.customer = customer;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
