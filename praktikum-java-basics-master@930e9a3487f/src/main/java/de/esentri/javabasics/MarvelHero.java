e47e7d648335e399274fd31f096d6a3c8ff5ef34
sStephen Kamau <stephenkamau@esentri.com>
2021-11-23 08:41:51 +0100

    Update MarvelHero.java

--- praktikum-java-basics-master@930e9a3487f/src/main/java/de/esentri/javabasics/MarvelHero.java
+++ praktikum-java-basics-master@930e9a3487f/src/main/java/de/esentri/javabasics/MarvelHero.java
@@ -8,6 +8,7 @@ public class MarvelHero {
   private Alignment alignment;
   private String year;
 
+  //K0nstruktor
   public MarvelHero(int page_id, String name, Gender gender, Alignment alignment, String year) {
     this.page_id = page_id;
     this.name = name;
@@ -16,50 +17,72 @@ public class MarvelHero {
     this.year = year;
   }
 
+  //Methode
   public MarvelHero() {
+
   }
 
+  //Getter
   public int getPage_id() {
+
     return page_id;
   }
 
+  //Setter
   public void setPage_id(int page_id) {
+
     this.page_id = page_id;
   }
 
+  //Getter
   public String getName() {
+
     return name;
   }
 
+  //Setter
   public void setName(String name) {
+
     this.name = name;
   }
 
+  //Getter
   public Gender getGender() {
+
     return gender;
   }
 
+  //Setter
   public void setGender(Gender gender) {
+
     this.gender = gender;
   }
 
+  //Getter
   public Alignment getAlignment() {
+
     return alignment;
   }
 
+  //Setter
   public void setAlignment(Alignment alignment) {
+
     this.alignment = alignment;
   }
 
+  //Getter
   public String getYear() {
+
     return year;
   }
 
+  //Setter
   public void setYear(String year) {
     this.year = year;
   }
 }
 
+//festgelegt Variablen.
 enum Gender {
 
   MALE_CHARACTERS,
@@ -67,6 +90,7 @@ enum Gender {
   AGENDER_CHARACTERS
 }
 
+//festgelegt Variablen.
 enum Alignment {
   BAD_CHARACTERS,
   GOOD_CHARACTERS,
