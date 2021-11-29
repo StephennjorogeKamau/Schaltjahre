445df3742ca3f0e0d8507f0cda2938dfa4f7e63e
sStephen Kamau <stephenkamau@esentri.com>
2021-11-29 12:59:28 +0100

    Update SteveDemoApplication.java

--- steve_demo/src/main/java/com/example/steve_demo/SteveDemoApplication.java
+++ steve_demo/src/main/java/com/example/steve_demo/SteveDemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.steve_demo;
 
+import com.example.steve_demo.student.Student1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,7 +18,13 @@ public class SteveDemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello" , "world");
+	public List<Student1> hello() {
+		return List.of(new Student1(
+				1l,
+				"Steve Kamau",
+				"steve.me@yahoo.com",
+		LocalDate.of(1987,01,10),
+		34
+		));
 	}
 }
