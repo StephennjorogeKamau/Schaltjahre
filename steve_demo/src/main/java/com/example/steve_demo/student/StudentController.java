84267597bda0cf8fe18ac45f8db581b1affef779
sStephen Kamau <stephenkamau@esentri.com>
2021-11-29 13:29:39 +0100

    Update StudentController.java

--- steve_demo/src/main/java/com/example/steve_demo/student/StudentController.java
+++ steve_demo/src/main/java/com/example/steve_demo/student/StudentController.java
@@ -1,4 +1,28 @@
 package com.example.steve_demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping(path = "api/v1/student1")
+
 public class StudentController {
+
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService){
+        this.studentService = studentService;
+    }
+    @GetMapping
+    public List<Student1> getStudents1() {
+        return studentService.getStudents1();
+    }
+
+
 }
