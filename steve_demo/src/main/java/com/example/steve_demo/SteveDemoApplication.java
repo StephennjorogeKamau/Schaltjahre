aca7bacef3f236e7d9d61ac14bce942018f3e5b6
sStephen Kamau <stephenkamau@esentri.com>
2021-11-29 11:43:47 +0100

    Create SteveDemoApplication.java

---
+++ steve_demo/src/main/java/com/example/steve_demo/SteveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.steve_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteveDemoApplication.class, args);
+	}
+
+}
