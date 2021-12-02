4772c327fd21f23f6d8c7adb3d0cafc7044a97b4
sStephen Kamau <stephenkamau@esentri.com>
2021-12-02 15:40:56 +0100

    Create Mitglied.java

---
+++ gk-integration-kreiswechsel-example-master/src/main/java/com/example/integration/model/Mitglied.java
@@ -0,0 +1,19 @@
+package com.example.integration.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Mitglied {
+  private long id;
+  private String username;
+  private String Firstname;
+  private String Lastname;
+  private String email;
+  private String phone;
+}
