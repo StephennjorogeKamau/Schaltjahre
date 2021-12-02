aa99fc6ad3f9634e11904ffcc2907211c914cc65
sStephen Kamau <stephenkamau@esentri.com>
2021-12-02 17:56:53 +0100

    Update KreisResourceClientTest.java

--- gk-integration-kreiswechsel-example-master/src/test/java/com/example/integration/boundary/clients/KreisResourceClientTest.java
+++ gk-integration-kreiswechsel-example-master/src/test/java/com/example/integration/boundary/clients/KreisResourceClientTest.java
@@ -1,10 +1,7 @@
 package com.example.integration.boundary.clients;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import com.example.integration.model.Kreis;
+import com.example.integration.model.Mitglied;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +10,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -35,5 +36,33 @@ public class KreisResourceClientTest {
     assertEquals(HttpStatus.OK, response.getStatusCode());
     assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
     assertTrue(kreise.length == 2);
+  }
+
+  @Test
+  public void testReceiveAllMitgliederByKreisId() {
+    // given
+    int kreisId = 1;
+    // when
+    ResponseEntity<Mitglied[]> response = client.getAllMitgliederByKreis(kreisId);
+    Mitglied[] mitglieder = response.getBody();
+    // then
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    assertEquals(MediaType.APPLICATION_JSON, response.getHeaders().getContentType());
+    assertTrue(mitglieder.length == 2);
+  }
+
+  @Test
+  public void testUpdateMitgliederByMitgliedId() {
+    // given
+    int currentId = 420;
+
+    Mitglied testMitglied = new Mitglied();
+    testMitglied.setId(7);
+    testMitglied.setEmail("matthias.schnell@esentri.com");
+    testMitglied.setName("matthias schnell");
+    // when
+    ResponseEntity<Void> response = client.updateMitgliedById(currentId, testMitglied);
+    // then
+    assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
   }
 }
