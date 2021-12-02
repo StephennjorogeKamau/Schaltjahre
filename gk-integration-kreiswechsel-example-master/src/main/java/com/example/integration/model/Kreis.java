94726337ee18d33d6896791dbb76d2ef2799304b
sStephen Kamau <stephenkamau@esentri.com>
2021-12-02 15:40:53 +0100

    Create Kreis.java

---
+++ gk-integration-kreiswechsel-example-master/src/main/java/com/example/integration/model/Kreis.java
@@ -0,0 +1,17 @@
+package com.example.integration.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Kreis {
+  private long id;
+  private String name;
+  private String mitglieder;
+}
