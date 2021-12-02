3893cddd751791ae9db6ed56ec03a776b1660a32
sStephen Kamau <stephenkamau@esentri.com>
2021-12-02 15:40:49 +0100

    Create KreisResourceClient.java

---
+++ gk-integration-kreiswechsel-example-master/src/main/java/com/example/integration/boundary/clients/KreisResourceClient.java
@@ -0,0 +1,26 @@
+package com.example.integration.boundary.clients;
+
+import com.example.integration.model.Kreis;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+public class KreisResourceClient {
+
+  public KreisResourceClient() {}
+
+  private static final String LOCAL_URL = "http://0.0.0.0:9091/v1/mockService";
+  private static final String REST_RESOURCE_KREIS = "/kreis";
+
+  @Autowired private RestTemplate restTemplate;
+
+  public ResponseEntity<Kreis[]> getAllKreise() {
+    log.info("Calling Rest Service: {}{}", LOCAL_URL, REST_RESOURCE_KREIS);
+
+    return restTemplate.getForEntity(LOCAL_URL + REST_RESOURCE_KREIS, Kreis[].class);
+  }
+}
